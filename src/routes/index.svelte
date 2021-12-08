d0a5953f87008fd4f69183a702b15ae11d48c0a2
alexandre <alexandre@pandascore.co>
2021-12-08 17:11:59 +0100

    faster load, rm montserrat

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<svelte:head>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-</svelte:head>
-
 <script>
 
   import { FingerPrint, Check, X, Exclamation, Trash, ChartPie, Save, Upload} from 'svelte-hero-icons'
@@ -116,7 +112,7 @@
     {#each listWords as word, i}
     <div>
       <div id={word.id} on:click={updateIndexMouse} class="justify-self-stretch pt-1 pb-1 pl-3 {word.id == index ? 'border-gray-500' : 'border-gray-200'} border self-stretch relative rounded-md shadow-sm">
-        <p id={word.id} on:click={updateIndexMouse} class="font-medium block w-full pr-10 sm:text-sm rounded-md {word.text == 'rouge' ? 'text-red-600 font-semibold' : 'text-gray-600'}">
+        <p id={word.id} on:click={updateIndexMouse} class="block w-full pr-10 sm:text-sm rounded-md {word.text == 'rouge' ? 'text-red-600 font-semibold' : 'text-gray-600'}">
           {word.text}
         </p>
         <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,6 @@
 const config = {
   mode: 'jit',
   purge: ['./src/**/*.{html,js,svelte,ts}'],
-
-  theme: {
-    fontFamily: {
-         'sans': ['Montserrat', 'ui-sans-serif', 'system-ui']
-        },
-    extend: {}
-  },
-
   plugins: [
   require('@tailwindcss/forms')
   ]
